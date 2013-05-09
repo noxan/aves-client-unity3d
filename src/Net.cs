1ff5e1b546c8e08c4f585efba3e68b31bbe2a561
noxan <noxan@byteweaver.net>
2013-05-09 12:56:32 +0200

    implement read thread with buffer

--- src/Net.cs
+++ src/Net.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Net.Sockets;
 using System.Net;
+using System.Text;
 using System.Threading;
 
 
 public class Net {
+    public const int BUFFER_SIZE = 1024;
+
+    private byte[] byteBuffer = new byte[BUFFER_SIZE];
     private TcpClient tcpClient;
     private NetworkStream stream;
 
@@ -21,10 +25,31 @@ public class Net {
             tcpClient = new TcpClient();
             tcpClient.Client.Connect("localhost", 1666);
             stream = tcpClient.GetStream();
+
+            readThread = new Thread(new ThreadStart(ReadThread));
+            readThread.Start();
         } catch(SocketException socketException) {
             Logger.Log("Connect: SocketException " + socketException);
         } catch(Exception exception) {
             Logger.Log("Connect: Exception " + exception);
+        }
+    }
+
+    private void ReadThread() {
+        while(true) {
+            try {
+                if(stream.DataAvailable) {
+                    int num = stream.Read(byteBuffer, 0, BUFFER_SIZE);
+                    if(num < 1) {
+                        Logger.Log("Read: End of stream, connection closed.");
+                        break;
+                    }
+                    string data = Encoding.ASCII.GetString(buf, 0, size);
+                }
+            } catch(Exception exception) {
+                Logger.Log("Read: " + exception);
+                break;
+            }
         }
     }
 }
