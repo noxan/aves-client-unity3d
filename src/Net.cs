026b4c258fe12d6897ce79e04f6fef18d952362a
noxan <noxan@byteweaver.net>
2013-05-09 12:54:34 +0200

    implement connect and connect thread

--- src/Net.cs
+++ src/Net.cs
@@ -1,2 +1,30 @@
+using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+
 public class Net {
+    private TcpClient tcpClient;
+    private NetworkStream stream;
+
+    private Thread connectThread;
+    private Thread readThread;
+
+    public void Connect() {
+        connectThread = new Thread(new ThreadStart(ConnectThread));
+        connectThread.Start();
+    }
+
+    private void ConnectThread() {
+        try {
+            tcpClient = new TcpClient();
+            tcpClient.Client.Connect("localhost", 1666);
+            stream = tcpClient.GetStream();
+        } catch(SocketException socketException) {
+            Logger.Log("Connect: SocketException " + socketException);
+        } catch(Exception exception) {
+            Logger.Log("Connect: Exception " + exception);
+        }
+    }
 }
