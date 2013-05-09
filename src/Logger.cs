1b29dbb207bc5342862beb73203ff30a853fced2
noxan <noxan@byteweaver.net>
2013-05-09 12:24:05 +0200

    change singleton pattern

--- src/Logger.cs
+++ src/Logger.cs
@@ -2,15 +2,12 @@ using System.IO;
 
 
 public class Logger {
-    private static readonly Logger instance = new Logger();
+    private static readonly StreamWriter writer = File.AppendText(@".\aves-network.log");
 
-    private StreamWriter writer;
+    private Logger() {}
 
-    private Logger() {
-        writer = File.AppendText(@".\aves-network.log");
-    }
 
     public static void Log(string message) {
-        instance.writer.WriteLine(message);
+        writer.WriteLine(message);
     }
 }
